22753707b57ac1ded9054a28284ba1fd47d6fe9d
Nick Nichols <nick.nichols@rate.com>
2019-11-20 08:15:27 -0600

    Version bump

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## v0.2.0 / 2019 November 20
 
+> This release fixes project #
+
+## v0.2.0 / 2019 November 20
+
 > This release adds `->yes-no` and `dissoc-in`
 
 ## v0.1.0 / 2019 November 16
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nnichols "0..0"
+(defproject nnichols "0.2.1"
             :description "A bunch of functions and definitions I'm sick of copy/pasting"
             :url "https://github.com/nnichols/nnichols"
             :license {:name "MIT"
